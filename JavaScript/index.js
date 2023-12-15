1cc4afc95fb763e4076978e94bea644eafe10526
Felipe Santa Cruz <felipesantacruz.ads@gmail.com>
2023-12-15 19:58:42 -0300

    projeto1

---
+++ JavaScript/index.js
@@ -0,0 +1,10 @@
+let elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida){
+  duvida.addEventListener('click', function(){
+    duvida.classList.toggle('ativa')
+  })
+})
+
+
+
